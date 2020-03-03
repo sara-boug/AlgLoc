13dffdb8251892dace89da5a9b22fe18229df687
Sara bouglam <sarabouglam@gmail.com>
2020-03-03 18:48:33 +0100

    fulfilling pagination part for state modification part

--- js/pagination.js
+++ js/pagination.js
@@ -1,90 +1,180 @@
-$(document).ready(function(){
-  var data = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque","Akron", "Albany", "Albuquerque"];
-  // modifying the initial elelment
-  var stateContainer= document.getElementById('addState');
-  //looping through the arrray and modifying the input
-  document.getElementById('OneState').childNodes[1].value= data[0];
-  data.forEach((item , i, array)=>{
-    if(i!=array.length-1) {
-      var oneState= document.getElementById('OneState').cloneNode(true);
-      oneState.childNodes[1].value= array[i+1];
-      stateContainer.appendChild(oneState);
+(function(window) {
+
+ function  Pagination() {
+    var pagination = {};
+    pagination.setting={
+        data:[],
+        partContainer:null,
+        onePart: null,
+        pagination: null,
+        limit:0,
+        pagination_array_length:0,
+        maxnum :0,
+        numbersContainer:null,
+        arrowSelector:null
     }
-    return;
-  });
 
-  var pagination= document.querySelector(".pagination .numbers");
-  var limit= 4;
-  // the pages left according  to the number of data
-  var number_of_pages=Math.floor(50/limit)+data.length%limit;
-  var pagination_array_length= 7;
+    pagination.forming_pagination = () =>  {
 
-   paginationtNumber(50,7);
+       pagination.setting.maxNum=pagination.fillArrayData(this.setting.data, this.setting.limit); // number of pages that shoulf be displayed according to the  available data
+       pagination.setting.pagNum=pagination.fillArray(pagination.setting.maxNum.length,pagination.setting.pagination_array_length);
+       console.log(pagination.setting.pagNum);
+       pagination.displayData(pagination.setting.maxNum[0].data)
+       pagination.paginationNumber(pagination.fillArray(pagination.setting.maxNum.length,pagination.setting.pagination_array_length) , pagination.setting.pagination);
+       pagination.setting.pagination.firstElementChild.style.backgroundColor="rgb(4, 127, 140)";
 
+       pagination.handleClick(pagination.setting.maxNum);
 
-  function paginationtNumber(maxNum,pagination_array_length) {
-    var iterations= Math.floor(maxNum/pagination_array_length);
-    var numbers=[];
-    var i=1;
-    var j =1;
-    var max=0;
+   }
 
-   // initialising the pagination numbers
-    for(i; i<=pagination_array_length; i++){
-      numbers.push(i.toString())
+  pagination.fillArray = (maxNum,pagination_array_length) => {
+  var iterations= Math.floor(maxNum/pagination_array_length);
+  var numbers=[];
+  var subNum=[];
+  var i=1;
+  var j =1;
+  var max=0;
+  if(maxNum-pagination_array_length>0) {
+    for(var i=1; i<=maxNum; i++){
+      subNum.push(i.toString());
+      if(i%pagination_array_length==0){
+        numbers.push(subNum);
+        subNum=[]
+      } if(i==maxNum-maxNum%pagination_array_length){
+        for(i=i+1; i<=maxNum ; i++){
+          subNum.push(i.toString());
+        }
+        numbers.push(subNum);
+        break;
+      }
     }
-    entity(numbers);
-
-    document.querySelectorAll("#pagination i").forEach( function(item) {
-      item.addEventListener("click",function(e){
-        numbers=[];
-        if (item.id=="left") {
-          if(j==1){
-            return;
-          } else {
-          // for i we move two steps howe ever for  max we move  by one step
-          i=i-(pagination_array_length*2);
-          max=max-pagination_array_length;
-          j=j-1;
-          console.log(j);
-        }
-        }
-        if (item.id=="right"){
-          if(j> iterations){
-            return;
-          } else {
-          max=j*pagination_array_length+pagination_array_length;
-          if(j== iterations){
-            max= 50;
-          }
-          j=j+1;
-        }
-        }
-        remove(pagination);
-        for(i; i<=max;i++){
-          numbers.push(i.toString());
-        }
-        entity(numbers);
-      });
-    })
-
+  } else {
+    for(var i=1; i<=maxNum; i++){
+      subNum.push(i.toString());
+    }
+    numbers.push(subNum);
   }
-  // visualising the pagination numbers
-  function entity(numbers){
-    numbers.forEach((item, i) => {
 
-      var element= document.createElement("a")
-      element.innerHTML=item;
-      pagination.appendChild(element);
+  return numbers;
+}
+
+ pagination.paginationNumber = (array , pag)=>{
+  var length= array.length;
+  var j=0;
+
+  // initialising the pagination numbers
+  pagination.entity(array[j]);
+  document.querySelectorAll(pagination.setting.arrowSelector).forEach( function(item) {
+
+    item.addEventListener("click",function(e){
+      document.querySelectorAll(pagination.setting.arrowSelector).forEach( function(item) { item.style.color="white";});
+      if (item.id=="left") {
+        if(j>=1){  j=j-1;  } else {j=0 ;
+            item.style.color="gray"; }
+            remove(pagination.setting.partContainer);
+            pagination.displayData(pagination.setting.maxNum[j].data);
+
+      }
+      if(item.id=="right"){
+        if(j<array.length-1){  j=j+1; }else { j=array.length-1 ; item.style.color="gray"; }
+        remove(pagination.setting.partContainer);
+        pagination.displayData(pagination.setting.maxNum[j].data);
+      }
+      pagination.remove(pag);
+      pagination.entity(array[j]).style.backgroundColor="rgb(4, 127, 140)";
+      pagination.handleClick(pagination.setting.maxNum);
+
     });
-
+  })
+}
+// visualising the pagination numbers
+  pagination.entity = (numbers) => {
+  numbers.forEach((item, i) => {
+    var element= document.createElement("a")
+    element.innerHTML=item;
+    pagination.setting.pagination.appendChild(element);
+  });
+  return pagination.setting.pagination.firstElementChild;
+}
+// removing the numbers each time
+ pagination.remove = (node)=> {
+  var child =  node.lastElementChild;
+  while(child && child !=null){
+    node.removeChild(child);
+    child = node.lastElementChild;
   }
-  // removing the numbers each time
-  function remove(node){
-    var child =  node.lastElementChild;
-    while(child && child !=null){
-      node.removeChild(child);
-      child = node.lastElementChild;
+}
+
+  pagination.fillArrayData = (data, limit)=>{
+  //var iterations= Math.floor(maxDataNum/limit);
+  var wholeData=[];
+  var subData=[];
+  var i=1;
+  var j =1;
+  var max=0;
+  for(var i=0; i<data.length; i++){
+    subData.push(data[i]);
+    if(i%limit==0 && i!=0) {
+      wholeData.push({
+        data:subData ,
+        index:j
+      });
+      j=j+1;
+      subData=[]
+    } if(i==data.length-data.length%limit ){
+      for(i; i<data.length; i++){
+        subData.push(data[i]);
+      }
+      wholeData.push({
+        data:subData,
+        index:j
+      });
+      break;
     }
   }
-});
+   overallData=wholeData;
+   return wholeData;
+}
+
+pagination.displayData=(data) => {
+  pagination.setting.onePart.remove();//deleting this part in order to remove the empty input in the begining
+  data.forEach((item , i, array)=>{
+     if(i!=array.length) {
+         var oneState=pagination.setting.onePart.cloneNode(true);
+         pagination.template(oneState,array[i]);
+         pagination.setting.partContainer.appendChild(oneState);
+     }
+ });
+}
+
+ pagination.handleClick = (data ) => {
+   document.querySelectorAll(pagination.setting.numbersContainer+" a").forEach( function(item) {
+    item.addEventListener("click", () => {
+      pagination.stylePagiNumbers(item);
+      remove(pagination.setting.partContainer);
+      for (var i = 0; i <data.length; i++) {
+        if(data[i].index==parseInt(item.innerHTML)){
+             displayedData=data[i].data;
+             pagination.displayData(data[i].data);
+          return data[i].data;
+        }
+      }
+    });
+  })
+}
+
+pagination.stylePagiNumbers= (item)=>{
+  document.querySelectorAll(pagination.setting.numbersContainer+" a").forEach( function(item) {
+    item.style.backgroundColor="rgb(4, 127, 140,0)" ;
+  });
+  item.style.backgroundColor="rgb(4, 127, 140,1)" ;
+  }
+
+return pagination;
+}
+
+
+if(typeof (window.pagination=== undefined)) {
+           window.pagination=Pagination();
+}
+})(window)
